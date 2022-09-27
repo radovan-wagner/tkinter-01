c7f624b610ccd7cf055f297214f72b08e44b5151
Radovan Wagner <radovan.wagner@gmail.com>
2022-09-27 19:47:30 +0200

    Hello World !

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import tkinter as tk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from thinter import ttk
 
+root = tk.Tk()              # Main Window
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ttk.label( root, text="Nazdar Radenko !" ).pack()
 
+root.mainloop()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
