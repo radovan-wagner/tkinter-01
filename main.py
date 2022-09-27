efa82047f6ef2ffbc42773920a22d726bfb9a58e
Radovan Wagner <radovan.wagner@gmail.com>
2022-09-27 19:58:58 +0200

    Aj button aj padding. Aj Padding Hello World !

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import tkinter as tk
 
 from tkinter import ttk
 
+def greet():
+    print( "Hello World !" )
+
 root = tk.Tk()              # Main Window
 
+
 ttk.Label( root, text="Nazdar Radenko !", padding=(30,10) ).pack()
+
+greet_button = ttk.Button( root, text="Pozdravujem !", command=greet, padding=(30,10) )
+greet_button.pack()
 
 root.mainloop()
 
